85139556d964e995a3ff2714c33a31c13c5671a4
RigvedWaradpande <rigved.waradpande@gmail.com>
2024-06-25 22:18:59 +0530

    fixed unable to break out of while loop issue

--- main.c
+++ main.c
@@ -32,9 +32,10 @@ void enableRawMode(){
 
 int main(){
 	enableRawMode();
-
+	
+	char prev ='\0';
 	while(1){
-		char c = '\0', prev = '\0';
+		char c = '\0';
 	        if(read(STDIN_FILENO, &c, 1)==-1 && errno != EAGAIN) die("read");
 		if(iscntrl(c)) printf("%d\r\n", c);
 		else printf("%d('%c')\r\n", c, c);
