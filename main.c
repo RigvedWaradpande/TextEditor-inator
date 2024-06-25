35afbcefd96428808ae0095d12b1bfa2703f10ed
RigvedWaradpande <rigved.waradpande@gmail.com>
2024-06-25 21:30:48 +0530

    disabled all useful and useless flags

--- main.c
+++ main.c
@@ -9,8 +9,13 @@ void disableRawMode(){
 void enableRawMode(){
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disableRawMode);
+
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO| ICANON);
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -19,8 +24,8 @@ int main(){
 
 	char c, prev = 0;
 	while(read(STDIN_FILENO, &c, 1) == 1 && prev != 'j' && c != 'q'){
-		if(iscntrl(c)) printf("%d\n", c);
-		else printf("%d('%c')\n", c, c);		
+		if(iscntrl(c)) printf("%d\r\n", c);
+		else printf("%d('%c')\r\n", c, c);		
 		prev = c;
 	}
 	return 0;
